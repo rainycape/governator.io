3d66957617e85f20026673ce0ba3a7941503fbe7
Alberto García Hierro <alberto@garciahierro.com>
2014-02-19 20:55:37 +0000

    Add section about logging in the documentation

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"gnd.la/app"
 	_ "gnd.la/bootstrap"
 	"gnd.la/config"
+	"gnd.la/html"
+	"gnd.la/template"
 	"gnd.la/util"
 	"net/http"
 	"os"
@@ -34,6 +38,20 @@ func fileHandler(name string) app.Handler {
 }
 
 func init() {
+	template.AddFuncs(template.FuncMap{
+		"#config": func(name string, def string) template.HTML {
+			var buf bytes.Buffer
+			buf.WriteString("<h3 class=\"config\">")
+			buf.WriteString(html.Escape(name))
+			if def != "" {
+				buf.WriteString(" <span class=\"label label-success\">optional</span>")
+				fmt.Fprintf(&buf, " <span class=\"default\">default: %s</span>", html.Escape(def))
+			} else {
+				buf.WriteString(" <span class=\"label label-danger\">required</span>")
+			}
+			buf.WriteString("</h3>")
+			return template.HTML(buf.String())
+		}})
 	config.MustParse(&Config)
 	App = app.New()
 	App.SetTrustXHeaders(true)
