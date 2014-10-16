48e709e66c78101820bc86ed01b0610bb7f5944e
Alberto García Hierro <alberto@garciahierro.com>
2014-10-16 14:35:34 +0100

    Prepare for GAE deployment

--- main.go
+++ main.go
@@ -1,67 +1,9 @@
+// +build !appengine
+
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"gnd.la/app"
-	_ "gnd.la/bootstrap"
-	"gnd.la/config"
-	"gnd.la/html"
-	"gnd.la/template"
-	"gnd.la/util/pathutil"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
-)
-
-var (
-	Config config.Config
-	App    *app.App
-	data   = pathutil.Relative("data")
-)
-
-func fileHandler(name string) app.Handler {
-	p := filepath.Join(data, name)
-	return func(ctx *app.Context) {
-		f, err := os.Open(p)
-		if err != nil {
-			ctx.NotFound("")
-		}
-		defer f.Close()
-		var modtime time.Time
-		if fi, err := f.Stat(); err == nil {
-			modtime = fi.ModTime()
-		}
-		http.ServeContent(ctx, ctx.R, name, modtime, f)
-	}
-}
-
-func init() {
-	template.AddFuncs(template.FuncMap{
-		"#config": func(name string, def string) template.HTML {
-			var buf bytes.Buffer
-			buf.WriteString("<h3 class=\"config\">")
-			buf.WriteString(html.Escape(name))
-			if def != "" {
-				buf.WriteString(" <span class=\"label label-success\">optional</span>")
-				fmt.Fprintf(&buf, " <span class=\"default\">default: %s</span>", html.Escape(def))
-			} else {
-				buf.WriteString(" <span class=\"label label-danger\">required</span>")
-			}
-			buf.WriteString("</h3>")
-			return template.HTML(buf.String())
-		}})
-	config.MustParse(&Config)
-	App = app.New()
-	App.SetTrustXHeaders(true)
-	App.HandleAssets("/assets/", pathutil.Relative("assets"))
-	App.Handle("^/$", app.TemplateHandler("main.html", nil))
-	App.Handle("^/install\\.sh$", fileHandler("install.sh"))
-	App.Handle("^/get/releases/linux/x86_64/latest/governator$", fileHandler("governator"))
-	App.Handle("^/contrib/upstart/governator.conf$", fileHandler("governator.conf"))
-}
-
 func main() {
+	// Start listening on main(), so the app does not start
+	// listening when running tests.
 	App.MustListenAndServe()
 }
---
+++ main_appengine.go
@@ -0,0 +1,43 @@
+// +build appengine
+
+package main
+
+import (
+	"net/http"
+	"sync"
+	"time"
+
+	_ "gnd.la/admin"                 // required for make-assets command
+	_ "gnd.la/blobstore/driver/gcs"  // enable Google Could Storage blobstore driver
+	_ "gnd.la/cache/driver/memcache" // enable memcached cache driver
+	// Uncomment the following line to use Google Cloud SQL
+	//_ "gnd.la/orm/driver/mysql"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func _app_engine_app_init() {
+	// Make sure App is initialized before the rest
+	// of this function runs.
+	for App == nil {
+		time.Sleep(5 * time.Millisecond)
+	}
+	if err := App.Prepare(); err != nil {
+		panic(err)
+	}
+	http.Handle("/", App)
+	wg.Done()
+}
+
+// Only executed on the development server. Required for
+// precompiling assets.
+func main() {
+	wg.Wait()
+}
+
+func init() {
+	wg.Add(1)
+	go _app_engine_app_init()
+}
