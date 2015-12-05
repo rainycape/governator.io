cad3d88b1a7a481618c47f52c99dbe5b1980cbc7
Alberto García Hierro <alberto@garciahierro.com>
2015-12-05 14:44:11 +0000

    Don't use HandleAssets() to handle the contrib files
    It breaks the handling of the CSS/JS assets

--- app.go
+++ app.go
@@ -24,17 +24,22 @@ var (
 func fileHandler(name string) app.Handler {
 	p := filepath.Join(data, name)
 	return func(ctx *app.Context) {
-		f, err := os.Open(p)
-		if err != nil {
-			ctx.NotFound("")
-		}
-		defer f.Close()
-		var modtime time.Time
-		if fi, err := f.Stat(); err == nil {
-			modtime = fi.ModTime()
-		}
-		http.ServeContent(ctx, ctx.R, name, modtime, f)
+		serveFile(ctx, p)
 	}
+}
+
+func serveFile(ctx *app.Context, path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		ctx.NotFound("")
+		return
+	}
+	defer f.Close()
+	var modtime time.Time
+	if fi, err := f.Stat(); err == nil {
+		modtime = fi.ModTime()
+	}
+	http.ServeContent(ctx, ctx.R, filepath.Base(path), modtime, f)
 }
 
 func init() {
@@ -66,5 +71,8 @@ func init() {
 	App.Handle("^/install\\.sh$", fileHandler("contrib/install.sh"))
 	App.Handle("^/get/releases/linux/x86_64/latest/governator$", fileHandler("governator"))
 
-	App.HandleAssets("/contrib/", pathutil.Relative("data/contrib"))
+	App.Handle("^/contrib/(.*)", func(ctx *app.Context) {
+		path := filepath.Join(data, "contrib", filepath.FromSlash(ctx.IndexValue(0)))
+		serveFile(ctx, path)
+	})
 }
